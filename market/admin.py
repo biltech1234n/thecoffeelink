1de4ea5eef5c5ae1653d1c77b9d254795bf47446
biltech1234n <biltech1234n@gmail.com>
2025-12-05 22:12:33 +0300

    Update admin.py

--- market/admin.py
+++ market/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import SellerCertification
 
-# Register your models here.
+@admin.register(SellerCertification)
+class CertificationAdmin(admin.ModelAdmin):
+    list_display = ('seller', 'name', 'expiry_date', 'is_verified')
+    list_filter = ('is_verified', 'name')
+    search_fields = ('seller__user__username', 'authority_name')
+    actions = ['verify_certificates']
+
+    def verify_certificates(self, request, queryset):
+        queryset.update(is_verified=True)
+    verify_certificates.short_description = "Mark selected certificates as Verified"
